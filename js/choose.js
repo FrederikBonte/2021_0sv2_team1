2f1597f82be76bd98001c4c08c27b3b7589261ab
thijmen van dusschoten <0330433@student.rocvantwente.nl>
2021-10-12 11:35:50 +0200

    changed js file(added colors)

--- js/choose.js
+++ js/choose.js
@@ -9,19 +9,56 @@ function chooseColor(color, targetId="target")
 	{
 		case 1: {
 			// Apply the correct color.
-			target.style.backgroundColor = "red";
+			target.style.backgroundColor = "green";
 			target.style.color = "white";
 			break;
 		}
 		case 2: {
-			// Continue for all other colors...
-			// At least: yellow, green, blue, purple, black and white
-			// Let the other team know which color goes with each number!!!
+			// Apply the correct color.
+			target.style.backgroundColor = "purple";
+			target.style.color = "white";
+			break;
+		}
+		case 3: {
+			// Apply the correct color.
+			target.style.backgroundColor = "";
+			target.style.color = "white";
+			break;
+		}
+		case 4: {
+			// Apply the correct color.
+			target.style.backgroundColor = "yellow";
+			target.style.color = "white";
+			break;
+		}
+		case 5: {
+			// Apply the correct color.
+			target.style.backgroundColor = "white";
+			target.style.color = "white";
+			break;
+		}
+		case 6: {
+			// Apply the correct color.
+			target.style.backgroundColor = "black";
+			target.style.color = "white";
+			break;
+		}
+		case 7: {
+			// Apply the correct color.
+			target.style.backgroundColor = "blue";
+			target.style.color = "white";
+			break;
+		}
+		case 8: {
+			// Apply the correct color.
+			target.style.backgroundColor = "red";
+			target.style.color = "white";
+			break;
 		}
 		default: {
 			target.style.backgroundColor = "lightgray";
 			target.style.color = "darkgray";
 		}
-		
+
 	}
 }
